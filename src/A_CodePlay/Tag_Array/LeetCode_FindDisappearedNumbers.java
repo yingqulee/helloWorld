e22dc0d9d98d5c2154e63c3319e9f2839905031e
yingqulee <1595537136@qq.com>
2021-02-14 01:50:35 +0800

    update Array

---
+++ src/A_CodePlay/Tag_Array/LeetCode_FindDisappearedNumbers.java
@@ -0,0 +1,68 @@
+package A_CodePlay.Tag_Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 448. 找到所有数组中消失的数字 简单
+ *
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ *
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ *
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ *
+ * 输出:
+ * [5,6]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+ */
+public class LeetCode_FindDisappearedNumbers {
+
+    /**
+     *
+     * 找出 1 - n 中没有出现的数字。不能使用额外的空间，两次循环时间复杂度为 2O(n)，即为 O(n)。
+     *
+     * 解题思路：使用数组的下标来标记数字的出现于否，通过一遍遍历即可标记出全部已经出现的数组
+     *
+     * [4,3,2,7,8,2,3,1] 初始数据
+     *
+     * [4,3,2,-7,8,2,3,1] 第一个数据 4 出现，将数组的第四个也就是下标 3 的数据修改为负数。-7 计算时，通过绝对值处理一下即可不影响数据的计算
+     * [4,3,-2,-7,8,2,3,1]
+     * [4,-3,-2,-7,8,2,3,1]
+     * [4,-3,-2,-7,8,2,-3,1]
+     * [4,-3,-2,-7,8,2,-3,-1]
+     * [4,-3,-2,-7,8,2,-3,-1]
+     * [4,-3,-2,-7,8,2,-3,-1]
+     * [-4,-3,-2,-7,8,2,-3,-1]
+     *
+     * 计算结束，数组的第五个，第六个依然为整数，证明 5,6 没有出现
+     *
+     * @param nums
+     * @return
+     */
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            nums[Math.abs(nums[i]) - 1] = -Math.abs(nums[Math.abs(nums[i]) - 1]);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res.add(i+1);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        LeetCode_FindDisappearedNumbers l = new LeetCode_FindDisappearedNumbers();
+        System.out.println(l.findDisappearedNumbers(nums));
+    }
+}
