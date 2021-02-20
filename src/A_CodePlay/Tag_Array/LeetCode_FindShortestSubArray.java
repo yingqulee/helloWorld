0fcc1b4d573f338e61447fe15fbed3bc7c672aca
lyqu <7713427+lyqu@user.noreply.gitee.com>
2021-02-20 14:06:44 +0800

    update Array and Sliding Window

---
+++ src/A_CodePlay/Tag_Array/LeetCode_FindShortestSubArray.java
@@ -0,0 +1,56 @@
+package A_CodePlay.Tag_Array;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 697. 数组的度 简单
+ * 给定一个非空且只包含非负数的整数数组 nums，数组的度的定义是指数组里任一元素出现频数的最大值。
+ *
+ * 你的任务是在 nums 中找到与 nums 拥有相同大小的度的最短连续子数组，返回其长度。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：[1, 2, 2, 3, 1]
+ * 输出：2
+ * 解释：
+ * 输入数组的度是2，因为元素1和2的出现频数最大，均为2.
+ * 连续子数组里面拥有相同度的有如下所示:
+ * [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+ * 最短连续子数组[2, 2]的长度为2，所以返回2.
+ * 示例 2：
+ *
+ * 输入：[1,2,2,3,1,4,2]
+ * 输出：6
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/degree-of-an-array
+ */
+public class LeetCode_FindShortestSubArray {
+
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        Map<Integer, Integer> left = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!left.containsKey(nums[i])) {
+                left.put(nums[i], i);
+            }
+            right.put(nums[i], i);
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int res = nums.length;
+        int degree = Collections.max(count.values());
+        for (int key : count.keySet()) {
+            if (degree == count.get(key)) {
+                res = Math.min(res, right.get(key) - left.get(key) + 1);
+            }
+        }
+        return res;
+    }
+}
